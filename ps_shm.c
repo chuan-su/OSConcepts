e2e46b134fd7dafb71e9969a67d8afaad5e77d17
chuan-su <chuansu@icloud.com>
2018-04-27 22:37:51 +0200

    shared memeory between parent and child process

---
+++ ps_shm.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+int main() {
+  const int SIZE = 4096;
+  const char *name = "OS";
+  const char * message_0 = "Hello";
+  const char * message_1 = "Process!";
+
+  int pid;
+
+  int shm_fd;
+  void *ptr;
+
+  shm_fd = shm_open(name, O_CREAT | O_RDONLY, 0666);
+  ftruncate(shm_fd, SIZE);
+  ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+
+  pid = fork();
+
+  if (pid < 0) {
+    fprintf(stderr, "Fork Failed\n");
+    return 1;
+  } else if (pid == 0) {
+    shm_fd = shm_open(name, O_RDWR, 0666);
+    ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    sprintf(ptr, "%s", message_0);
+    ptr += strlen(message_0);
+    sprintf(ptr, "%s", message_1);
+    ptr += strlen(message_1);
+  } else {
+    wait(NULL);
+    printf("%s\n", (char *) ptr);
+    shm_unlink(name);
+  }
+
+  return 0;
+}
